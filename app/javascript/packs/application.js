6cb5f1ecb64c706786ddac0cdb6c93c685b67a24
Angel Mendez <angelmendezz751@gmail.com>
2020-10-01 23:19:23 -0500

    feat(Product): Add choices for multiple select

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import Choices from "choices.js";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+document.addEventListener("turbolinks:load", () => {
+    const element = document.querySelector('.js-choice');
+    if (element) {
+        const choices = new Choices(element);
+    }
+})
